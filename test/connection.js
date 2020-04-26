bff7c70430a2d0df399382fa2c7ff29d003dcfc9
David <41311088+biagiola@users.noreply.github.com>
2020-04-26 15:26:39 -0400

    Add files via upload

--- test/connection.js
+++ test/connection.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
-// Connect to mongodb
-const uri = 'mongodb://localhost/testaroo';
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+// Connecto to the db before test run
+before(function(done){
+    // Connect to mongodb
+    const uri = 'mongodb://localhost/testaroo';
+    mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 
-mongoose.connection.once('open', function(){
-    console.log('Connection has been made, now make fireworks...');
-}).on('error', function(error){
-    console.log('Connection error:', error);
+    mongoose.connection.once('open', function(){
+        console.log('Connection has been made, now make fireworks...');
+        done();
+    }).on('error', function(error){
+        console.log('Connection error:', error);
+    });    
 });
+
