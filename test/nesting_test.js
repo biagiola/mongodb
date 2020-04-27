d05de02c8cf62862cd8f5c51e759cca64246cea8
David <41311088+biagiola@users.noreply.github.com>
2020-04-27 10:20:39 -0400

    Add files via upload

---
+++ test/nesting_test.js
@@ -0,0 +1,53 @@
+const assert = require('assert');
+const mongoose = require('mongoose');
+const Author = require('../models/author');
+
+// Describe our tests
+describe('Nesting records', function(){
+
+    beforeEach(function(done){
+        mongoose.connection.collections.authors.drop(function(){
+            done();
+        });
+    });
+
+    // Create tests
+    it('Creates an author with sub.documents', function(done){
+
+        var pat = new Author({
+            name: 'Patrick Rothfuss',
+            books: [{ title: 'Name of the wind', pages: 400 }]
+        });
+
+        pat.save().then(function(){
+            Author.findOne({ name: 'Patrick Rothfuss'}).then(function(record){
+                assert(record.books.length === 1);
+                done();
+            });
+        });
+    });
+
+    // Another test to add more books 
+    it('Adds a book to an author', function(done){
+        
+        var pat = new Author({
+            name: 'Patrick Rothfuss',
+            books: [{title: 'Name of the wind', pages: 400 }]
+        });
+
+        pat.save().then(function(){
+
+            Author.findOne({ name: 'Patrick Rothfuss'}).then(function(record){
+                // add a book to the books array
+                record.books.push({title: "Wise man's fear", page: 500});
+                record.save().then(function(){
+
+                    Author.findOne({ name: 'Patrich Rothfuss' }).then(function(result){
+                        assert(record.books.length === 2);
+                        done();
+                    });
+                });
+            });
+        });
+    });
+});
