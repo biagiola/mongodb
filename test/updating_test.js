ad01ca952bae7d78f6a41aaa2836636803d9b726
David <41311088+biagiola@users.noreply.github.com>
2020-04-27 08:19:03 -0400

    Add files via upload

---
+++ test/updating_test.js
@@ -0,0 +1,32 @@
+const assert = require('assert');
+const MarioChar = require('../models/marioChar');
+
+
+// Describe tests
+describe('Updating records', function(){
+
+    var char;
+
+    beforeEach(function(done){
+        char = new MarioChar({
+            name: 'Mario'
+        });
+
+        char.save().then(function(){
+            assert(char.isNew === false);
+            done();
+        });
+    });
+
+    // Updating a record
+    it('Updates one record from the database', function(done){
+        
+        MarioChar.findOneAndUpdate({ name: 'Mario'}, { name: 'Luigi' }).then(function(){
+            MarioChar.findOne({ _id: char._id }).then(function(result){
+                assert(result.name === 'Luigi');
+                done();
+            })
+        })
+    });
+
+});
